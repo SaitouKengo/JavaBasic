a2180e211bf0ec98060f4c2c5ddce601258d1f6d
DESKTOP-PJDC9PU\LIKEIT_STUDENT <ken5550914@gmail.com>
2018-02-08 16:10:49 +0900

    11-3,12-1

---
+++ Practice/src/practice10/User.java
@@ -0,0 +1,17 @@
+package practice10;
+
+public class User {
+	int userId;
+	String userNm;
+	String mail;
+	String password;
+
+	void user(){
+		System.out.println(this.userId);
+		System.out.println(this.userNm);
+		System.out.println(this.mail);
+		System.out.println(this.password);
+
+	}
+
+}
