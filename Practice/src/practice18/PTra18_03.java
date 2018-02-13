e95470d298f2794a2b16bf711fc7e3e1c7b7273a
DESKTOP-PJDC9PU\LIKEIT_STUDENT <ken5550914@gmail.com>
2018-02-13 17:34:46 +0900

    18-4

--- Practice/src/practice18/PTra18_03.java
+++ Practice/src/practice18/PTra18_03.java
@@ -6,6 +6,13 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_03 {
 
 	/*
@@ -20,11 +27,42 @@ public class PTra18_03 {
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
 
+		ArrayList<Player> players = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                // ★ 1行ごとにArrayListに格納してください
+                Player player = new Player();
+                String[] lines = line.split(",", 0);
+
+                player.setPosition(lines[0]);
+                player.setName(lines[1]);
+                player.setCountry(lines[2]);
+                player.setTeam(lines[3]);
+
+                players.add(player);
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが見つかりません");
+        }
+
 
 		// ★ ①のArrayListの中から"レアル・マドリード", "バルセロナ"の選手を除外してください
+        int i = 0;
+        while(i < players.size()) {
+	        	if(players.get(i).getTeam().equals("レアル・マドリード")||players.get(i).getTeam().equals("バルセロナ")) {
+	        		players.remove(i);
+	        		continue;
+	        	}
+	        	i++;
+        }
 
 
 		// ★ 削除後のArrayListの中身を全件出力してください
+        for(Player p:players) {
+        		System.out.println(p.toString());
+        }
 
 
 	}
--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,15 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -19,9 +28,68 @@ public class PTra18_04 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		ArrayList<Player> players = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                // ★ 1行ごとにArrayListに格納してください
+                Player player = new Player();
+                String[] lines = line.split(",", 0);
+
+                player.setPosition(lines[0]);
+                player.setName(lines[1]);
+                player.setCountry(lines[2]);
+                player.setTeam(lines[3]);
+
+                players.add(player);
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが見つかりません");
+        }
 
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+        List<Player> list = players;
+        Collections.shuffle(list);
+        int[] count = {0,0,0,0};
+
+        for(int i=0;i<list.size();i++) {
+	        	if(list.get(i).getPosition().equals("GK")) {
+		        System.out.println(list.get(i));
+		        	count[0]++;
+		        	if(count[0]==1)
+		        	    break;
+	        	}
+        }
+
+        for(int i=0;i<list.size();i++) {
+	        if(list.get(i).getPosition().equals("DF")) {
+			    System.out.println(list.get(i));
+			    	count[1]++;
+			    	if(count[1]==4)
+			        break;
+		    	}
+        }
+
+        for(int i=0;i<list.size();i++) {
+	        if(list.get(i).getPosition().equals("MF")) {
+		        System.out.println(list.get(i));
+		        	count[2]++;
+		        	if(count[2]==4)
+				        break;
+	        }
+        }
+
+        for(int i=0;i<list.size();i++) {
+	        if(list.get(i).getPosition().equals("FW")) {
+		        System.out.println(list.get(i));
+		        	count[3]++;
+		        	if(count[3]==2)
+		        		break;
+	      	}
+	    }
+
 
 	}
 }
