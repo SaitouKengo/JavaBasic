ee65adc530a0e189d2b1a78157db27c6f36d36aa
DESKTOP-PJDC9PU\LIKEIT_STUDENT <ken5550914@gmail.com>
2018-02-13 15:48:37 +0900

    14-2

---
+++ Practice/src/practice14/ptra14/DBCourse.java
@@ -0,0 +1,19 @@
+package practice14.ptra14;
+
+import practice14.common.Course;
+
+public class DBCourse implements Course {
+
+	@Override
+	public String getCourseName() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public String[] getCourseUnit() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
---
+++ Practice/src/practice14/ptra14/javaCourse.java
@@ -0,0 +1,21 @@
+package practice14.ptra14;
+
+import practice14.common.Course;
+
+public class javaCourse implements Course {
+
+	@Override
+	public String getCourseName() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public String[] getCourseUnit() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+
+
+}
